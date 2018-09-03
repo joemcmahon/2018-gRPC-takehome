f32db8ed7f9e82faf0a4398e13edd4ee289f5756
Joe McMahon <joe.mcmahon@whitehatsec.com>
2018-09-03 00:53:42 -0700

    Repaired testing library, clean up server & test
     - The logrus log capture library used the old `Sirupsen` variant of the
       import path for logrus. Forked the library, fixed it, sent issue and
       PR with the fix to the author.
     - Vastly simplified the messaging in server for state transitions by
       wrapping them up in a small function to format the string properly.
    
    FIXME: Still have some failing tests.
    XXX:   Tomorrow, complete the gRPC portion of the code. Look at the
           automated test generation for gRPC to save time.
    XXX:   Need to add support for pausing crawls to the crawler; may
           possibly be able to isolate it to the Fetcher, which would be
           cleaner.
    XXX:   I think we can skip detailed tests for the generated Cobra code.

--- api/server.go
+++ api/server.go
@@ -1,6 +1,10 @@
 package Server
 
-import log "github.com/sirupsen/logrus"
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+)
 
 // Define a dummy crawl server for the first iteration.
 // This server will simply maintain state and not actually
@@ -36,23 +40,23 @@ func (c *CrawlServer) Start(url string) {
 	if state, ok := c.state[url]; ok {
 		switch state {
 		case running:
-			log.Infof("Start requested for running crawl on %s: no action", url)
+			log.Infof(changeState(url, "running", "running", "no action"))
 			c.state[url] = running
 		case done:
-			log.Infof("Start requested for completed crawl on %s: last crawl discarded, starting crawl", url)
+			log.Infof(changeState(url, "done", "running", "last crawl discarded, restarting crawl"))
 			c.state[url] = running
 		case stopped:
-			log.Infof("Start requested for stopped crawl on %s: resuming crawl", url)
+			log.Infof(changeState(url, "stopped", "running", "resuming crawl"))
 			c.state[url] = running
 		case failed:
-			log.Infof("Start requested for failed crawl on %s: retrying crawl", url)
+			log.Infof(changeState(url, "failed", "running", "retrying crawl"))
 			c.state[url] = running
 		default:
-			log.Infof("%s in invalid state %s: forcing stop", url, translate(state))
+			log.Infof(changeState(url, "invalid state", "running", "forcing stop"))
 			c.state[url] = stopped
 		}
 	} else {
-		log.Infof("Start requested for new URL %s: starting crawl", url)
+		log.Infof(changeState(url, translate(unknown), "running", "starting crawl"))
 		c.state[url] = running
 	}
 }
@@ -62,16 +66,16 @@ func (c *CrawlServer) Stop(url string) {
 	if state, ok := c.state[url]; ok {
 		switch state {
 		case running:
-			log.Infof("Stop requested for running crawl on %s: stopped crawl", url)
+			log.Infof(changeState(url, "running", "stopped", "crawl paused"))
 			c.state[url] = stopped
 		case done, stopped, failed:
-			log.Infof("Stop requested for %s crawl on %s: no action", translate(state), url)
+			log.Infof(changeState(url, translate(state), "stopped", "no action"))
 		default:
 			log.Infof("%s in invalid state %s: forcing stop", url, translate(state))
 			c.state[url] = stopped
 		}
 	} else {
-		log.Infof("Stop requested for unknown URL %s: no action", url)
+		log.Infof(changeState(url, translate(unknown), "stopped", "no action"))
 	}
 }
 
@@ -80,16 +84,29 @@ func (c *CrawlServer) Done(url string) {
 	if state, ok := c.state[url]; ok {
 		switch state {
 		case running:
-			log.Infof("Marking crawl of %s as done", url)
-			c.state[url] = stopped
+			log.Infof(changeState(url, "running", "done", "recording completed crawl"))
+			c.state[url] = done
 		default:
-			log.Infof("Attempt to mark crawl of %s in state %s as done: no action", url, translate(state))
-			c.state[url] = stopped
+			log.Infof(changeState(url, translate(state), "done", "no action"))
 		}
 	} else {
-		log.Infof("Stop requested for unknown URL %s: no action", url)
+		log.Infof(changeState(url, translate(unknown), "done", "no action"))
 	}
-	c.state[url] = done
+}
+
+// Failed marks a crawl as failed for a URL.
+func (c *CrawlServer) Failed(url string) {
+	if state, ok := c.state[url]; ok {
+		switch state {
+		case running:
+			log.Infof(changeState(url, translate(state), "failed", "marked failed"))
+			c.state[url] = failed
+		default:
+			log.Infof(changeState(url, translate(state), "failed", "no action"))
+		}
+	} else {
+		log.Infof(changeState(url, translate(unknown), "failed", "no action"))
+	}
 }
 
 // Probe checks the current state of a crawl without changing anything.
@@ -119,4 +136,8 @@ var xlate = map[crawlState]string{
 
 func translate(state crawlState) string {
 	return xlate[state]
+}
+
+func changeState(url, old, new, result string) string {
+	return fmt.Sprintf("Change %s in state %s to %s: %s", url, old, new, result)
 }
