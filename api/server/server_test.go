1cc579c18e1cf3d3d29e282d81a96177a13fecf7
Joe McMahon <joe.mcmahon@whitehatsec.com>
2018-09-26 10:08:52 -0700

    Fix the test API to match the real servr API
     - didn't commit actual updates to the field and methods names

--- api/server/server_test.go
+++ api/server/server_test.go
@@ -17,94 +17,94 @@ var _ = Describe("matches logs", func() {
 	s := New(f)
 	Context("running", func() {
 		It("tries change running to stopped", func() {
-			s.state[example] = CrawlControl{State: running}
+			s.crawlers[example] = CrawlControl{State: running}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Pause(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "running", "stopped", "crawl paused")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "running", "stopped", "crawl paused")))
 		})
 		It("tries change running to running", func() {
-			s.state[example] = CrawlControl{State: running}
+			s.crawlers[example] = CrawlControl{State: running}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Start(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "running", "running", "no action")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "running", "running", "no action")))
 		})
 	})
 	Context("stopped", func() {
 		It("tries change stopped to stopped", func() {
-			s.state[example] = CrawlControl{State: stopped}
+			s.crawlers[example] = CrawlControl{State: stopped}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Pause(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "stopped", "stopped", "no action")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "stopped", "stopped", "no action")))
 		})
 		It("tries change stopped to running", func() {
-			s.state[example] = CrawlControl{State: stopped}
+			s.crawlers[example] = CrawlControl{State: stopped}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Start(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "stopped", "running", "resuming crawl")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "stopped", "running", "resuming crawl")))
 		})
 	})
 	Context("unknown", func() {
 		//  Note that we use missing for these tests to
 		// trigger the "did not find the URL in the crawler state" cases.
 		It("tries change unknown to stopped", func() {
-			delete((*s).state, missing)
+			delete((*s).crawlers, missing)
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Pause(missing)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(missing, "unknown", "stopped", "no action")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(missing, "unknown", "stopped", "no action")))
 		})
 		It("tries change unknown to running", func() {
-			delete((*s).state, missing)
+			delete((*s).crawlers, missing)
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Start(missing)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(missing, "unknown", "running", "starting crawl")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(missing, "unknown", "running", "starting crawl")))
 		})
 	})
 	Context("failed", func() {
 		It("tries change failed to stopped", func() {
-			s.state[example] = CrawlControl{State: failed}
+			s.crawlers[example] = CrawlControl{State: failed}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Pause(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "failed", "stopped", "no action")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "failed", "stopped", "no action")))
 		})
 		It("tries change failed to running", func() {
-			s.state[example] = CrawlControl{State: failed}
+			s.crawlers[example] = CrawlControl{State: failed}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Start(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "failed", "running", "retrying crawl")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "failed", "running", "retrying crawl")))
 		})
 	})
 	Context("done", func() {
 		It("tries change done to stopped", func() {
-			s.state[example] = CrawlControl{State: done}
+			s.crawlers[example] = CrawlControl{State: done}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Pause(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "done", "stopped", "no action")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "done", "stopped", "no action")))
 		})
 		It("tries change done to running", func() {
-			s.state[example] = CrawlControl{State: done}
+			s.crawlers[example] = CrawlControl{State: done}
 			logHook := logcap.NewLogHook()
 			logHook.Start()
 			defer logHook.Stop()
 			s.Start(example)
-			Ω(logHook).Should(logcap.HaveLogs(changeState(example, "done", "running", "last crawl discarded, restarting crawl")))
+			Ω(logHook).Should(logcap.HaveLogs(s.changeState(example, "done", "running", "last crawl discarded, restarting crawl")))
 		})
 	})
 })
